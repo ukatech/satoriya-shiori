916b9e42445eb95ca81ec5d2c6f7704fac6c658a
radiowave.n <radiowave.n@98ed6b26-e130-0410-8773-31d6671a21de>
2007-05-23 13:49:46 +0000

    random()

--- satoriya/_/random.h
+++ satoriya/_/random.h
@@ -1,13 +1,13 @@
 #ifndef	RANDOM_H
 #define RANDOM_H
-
-void	randomize(unsigned int seed);
-int		random(void);
+#include <stdlib.h>
+//void	randomize(unsigned int seed);
+//int		random(void);
 inline int random(int max) {
 	if ( max <= 0 ) {
 		return 0;
 	}
-	return random()%max;
+	return static_cast<int>(random()%max);
 }
 
 #endif	// RANDOM_H
