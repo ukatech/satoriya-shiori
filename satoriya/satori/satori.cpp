aad01c5d5932f8a3de21d81ce5b1671221294005
C.Ponapalt <ponapalt@shillest.net>
2016-03-27 17:54:07 +0900

    Mc159-2

--- satoriya/satori/satori.cpp
+++ satoriya/satori/satori.cpp
@@ -14,7 +14,7 @@ const char* gSatoriName = "Satori";
 const char* gSatoriNameW = "—¢X";
 const char* gSatoriCraftman = "Yagi Kushigahama/The Maintenance Shop";
 const char* gSatoriCraftmanW = "‹ùƒ–•l‚â‚¬/®”õ”Ç";
-const char* gSatoriVersion = "phase Mc159-1";
+const char* gSatoriVersion = "phase Mc159-2";
 const char* gShioriVersion = "3.0";
 const char* gSaoriVersion = "1.0";
 
